62664568e20bf4a231e8aef230df6e6d54f1f293
Cliff Xuan <cliff.xuan@gmail.com>
2024-10-20 14:31:31 +0100

    Refactor run_shell_command to accept buffer creation function
    - Updated run_shell_command to take create_buffer_fn as an argument.
    - Adjusted key mappings for Git commands.

--- lua/gpt.lua
+++ lua/gpt.lua
@@ -1,7 +1,7 @@
 local utils = require 'utils'
 local Job = require 'plenary.job'
 
-local function create_buffer()
+local create_buffer = function()
   vim.cmd 'botright new'
   local buf = vim.api.nvim_get_current_buf()
   vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
@@ -10,7 +10,8 @@ local function create_buffer()
   return buf
 end
 
-local run_shell_command = function(input, shell_command)
+local run_shell_command = function(shell_command, input, create_buffer_fn)
+  create_buffer_fn = create_buffer_fn or create_buffer
   local buf = nil
   Job:new({
     command = 'sh',
@@ -21,7 +22,7 @@ local run_shell_command = function(input, shell_command)
       vim.schedule(function()
         if not buf then
           if line:gsub('^%s*(.-)%s*$', '%1') ~= '' then
-            buf = create_buffer()
+            buf = create_buffer_fn()
             vim.api.nvim_buf_set_lines(buf, 0, -1, false, { line })
           end
         else
@@ -32,7 +33,7 @@ local run_shell_command = function(input, shell_command)
     on_exit = function(j, code)
       vim.schedule(function()
         if code ~= 0 then
-          vim.api.nvim_buf_set_lines(buf or create_buffer(), 0, -1, false, j:stderr_result())
+          vim.api.nvim_buf_set_lines(buf or create_buffer_fn(), 0, -1, false, j:stderr_result())
           print 'error!'
         end
       end)
@@ -42,25 +43,30 @@ end
 
 vim.api.nvim_create_user_command('Gpt', function(prompt)
   utils.set_open_api_key()
-  run_shell_command(nil, 'sgpt ' .. '"' .. prompt.args .. '"')
+  run_shell_command('sgpt ' .. '"' .. prompt.args .. '"')
 end, { nargs = 1 })
 
 vim.api.nvim_create_user_command('GptVisual', function(prompt)
   utils.set_open_api_key()
-  run_shell_command(utils.get_visual_selection(), 'sgpt ' .. '"' .. prompt.args .. '"')
+  run_shell_command('sgpt ' .. '"' .. prompt.args .. '"', utils.get_visual_selection())
 end, { nargs = 1 })
 
 vim.api.nvim_create_user_command('GptCode', function(prompt)
   utils.set_open_api_key()
-  run_shell_command(nil, table.concat({ 'sgpt', '"' .. prompt.args .. '"', '--code' }, ' '))
+  run_shell_command(table.concat({ 'sgpt', '"' .. prompt.args .. '"', '--code' }, ' '))
 end, { nargs = 1 })
 
 vim.api.nvim_create_user_command('GptCodeVisual', function(prompt)
   utils.set_open_api_key()
-  run_shell_command(utils.get_visual_selection(), table.concat({ 'sgpt', '"' .. prompt.args .. '"', '--code' }, ' '))
+  run_shell_command(table.concat({ 'sgpt', '"' .. prompt.args .. '"', '--code' }, ' '), utils.get_visual_selection())
 end, { nargs = 1 })
 
 vim.api.nvim_create_user_command('GptGitCommitMsg', function()
   utils.set_open_api_key()
-  run_shell_command(nil, 'git diff --cached | sgpt "write a short git commit message" --no-md')
+  run_shell_command 'git diff --cached | sgpt "write a short git commit message" --no-md'
+end, { nargs = 0 })
+
+vim.api.nvim_create_user_command('GptGitDiffSummary', function()
+  utils.set_open_api_key()
+  run_shell_command 'git diff | sgpt "write a short git commit message" --no-md'
 end, { nargs = 0 })
