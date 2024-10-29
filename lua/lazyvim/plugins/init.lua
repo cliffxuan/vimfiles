182584d3109aa2f838f74f966c8a966ceafad2eb
Cliff Xuan <cliff.xuan@gmail.com>
2024-10-29 19:52:06 +0000

    render markdown

--- lua/lazyvim/plugins/init.lua
+++ lua/lazyvim/plugins/init.lua
@@ -89,10 +89,12 @@ return {
   { 'nvim-telescope/telescope.nvim', tag = '0.1.4' },
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   {
-    'MeanderingProgrammer/markdown.nvim',
-    ft = 'markdown',
-    main = 'render-markdown',
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
     opts = {},
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
   },
 }
