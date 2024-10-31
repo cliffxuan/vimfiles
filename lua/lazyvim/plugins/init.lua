7ae0885cbcff8b7ce34e1f5e83f6e767d74aa3bc
cliff xuan <cliff.xuan@gmail.com>
2024-10-31 16:07:57 +0000

    filetype

--- lua/lazyvim/plugins/init.lua
+++ lua/lazyvim/plugins/init.lua
@@ -107,6 +107,7 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, 
+    ft = 'terraform'
     opts = {},
   },
 }
