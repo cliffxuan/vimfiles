9b545f17c4e0738e08485a4bcbc13205bfa95a37
Cliff Xuan <cliff.xuan@gmail.com>
2024-11-01 12:59:59 +0000

    Updated buffer deletion mapping from 'delete_buffers' to 'delete_buffer'.

--- lua/lazyvim/plugins/telescope.lua
+++ lua/lazyvim/plugins/telescope.lua
@@ -1,27 +1,27 @@
-local actions = require('telescope.actions')
+local actions = require 'telescope.actions'
 
 return {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    config = function()
-        require('telescope').setup {
-        extensions = {
-            fzf = {
-            fuzzy = true,
-            override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = 'smart_case',
-            },
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  config = function()
+    require('telescope').setup {
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = 'smart_case',
         },
-        pickers = {
-            buffers = {
-                mappings = {
-                    n = {['dd'] = actions.delete_buffers,}
-                }
-            }
-        }
-        }
-        require('telescope').load_extension 'fzf'
-        vim.cmd 'autocmd User TelescopePreviewerLoaded setlocal number'
-    end,
+      },
+      pickers = {
+        buffers = {
+          mappings = {
+            n = { ['dd'] = actions.delete_buffer },
+          },
+        },
+      },
+    }
+    require('telescope').load_extension 'fzf'
+    vim.cmd 'autocmd User TelescopePreviewerLoaded setlocal number'
+  end,
 }
