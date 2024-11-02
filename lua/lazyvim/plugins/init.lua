24a638ea0d74eda479de2ae33dc636bc283ab2a5
Cliff Xuan <cliff.xuan@gmail.com>
2024-11-02 12:20:59 +0000

    Change filetype for render-markdown.nvim plugin from terraform to markdown

--- lua/lazyvim/plugins/init.lua
+++ lua/lazyvim/plugins/init.lua
@@ -89,7 +89,7 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
-    ft = 'terraform',
+    ft = 'markdown',
     opts = {},
   },
 }
