9b3654b57c7c62c2e959941a8b5b2fd82d48a6ee
Cliff Xuan <cliff.xuan@gmail.com>
2025-03-08 15:37:13 +0000

    feat: update Avante plugin configuration
    - Set provider to 'openai'
    - Configure file_selector to use 'telescope'

--- lua/lazyvim/plugins/avante.lua
+++ lua/lazyvim/plugins/avante.lua
@@ -4,7 +4,10 @@ return {
   lazy = false,
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
-    -- add any opts here
+    provider = 'openai',
+    file_selector = {
+      provider = 'telescope',
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = 'make',
@@ -41,13 +44,4 @@ return {
       ft = { 'markdown', 'Avante' },
     },
   },
-
-  config = function()
-    require('avante').setup {
-      provider = 'openai',
-      file_selector = {
-        provider = 'telescope',
-      },
-    }
-  end,
 }
