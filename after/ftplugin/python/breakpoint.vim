9b223b319ed1738e9cf381a39684691d6d3a32f6
Cliff Xuan <cliff.xuan@gmail.com>
2025-04-18 15:38:50 +0100

    Refactor Python breakpoint management
    - Added new breakpoint.vim for setting and removing breakpoints.
    - Removed outdated pdb.vim.
    - Introduced breakpoint.py for improved breakpoint handling with support for multiple debuggers.

---
+++ after/ftplugin/python/breakpoint.vim
@@ -0,0 +1,12 @@
+noremap  <LocalLeader>a :py set_breakpoint()<cr>:update<cr>
+noremap  <LocalLeader>p :py set_breakpoint('pdb')<cr>:update<cr>
+noremap <LocalLeader>b :py remove_breakpoints()<cr>:update<cr>
+
+python3 << EOF
+import vim
+import sys
+
+plugin_dir = vim.eval('stdpath("config")') + '/python'
+sys.path.insert(0, plugin_dir)
+from breakpoint import *
+EOF
---
+++ python/breakpoint.py
@@ -0,0 +1,47 @@
+import re
+
+import vim
+
+DEBUGGERS = ["pdb", "ipdb", "pudb"]
+
+
+def get_line(debugger=None):
+    if debugger is None:
+        statement = "breakpoint()"
+    else:
+        statement = f'__import__("{debugger}")'
+    return f"{statement}  # !!!!!!!!!!"
+
+
+def set_breakpoint(debugger=None):
+    n_line = int(vim.eval('line(".")'))
+    match = re.search(r"^(\s*)", vim.current.line)
+    if match is None:
+        vim.command("echoerr 'No whitespace found in the current line.'")
+        return
+    whitespace = match.group(1)
+    vim.current.buffer.append(whitespace + get_line(debugger), n_line - 1)
+
+
+def remove_breakpoints():
+    n_currentline = int(vim.eval('line(".")'))
+
+    n_lines = []
+    n_line = 1
+    for line in vim.current.buffer:
+        if line.lstrip() in [
+            get_line(),
+            *[get_line(debugger) for debugger in DEBUGGERS],
+        ]:
+            n_lines.append(n_line)
+        n_line += 1
+
+    n_lines.reverse()
+
+    for n_line in n_lines:
+        vim.command("normal %dG" % n_line)
+        vim.command("normal dd")
+        if n_line < n_currentline:
+            n_currentline -= 1
+
+    vim.command("normal %dG" % n_currentline)
